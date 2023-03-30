3acc65ade97696e920ad37a255124f2bda6f07e4
Ryan Peters <ryanpeters@microsoft.com>
2023-03-29 20:37:21 -0400

    refactor some crypto methods

--- CryptographyHelper.cs
+++ CryptographyHelper.cs
@@ -1,35 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.Json;
 
 public static class CryptographyHelper
 {
     // must be 16 bytes (128-bit)
     private static byte[] initVector = { 23, 45, 3, 56, 12, 128, 5, 34, 56, 124, 65, 23, 7, 34, 12, 49 };
-
-    public static async Task<string> EncryptToStringAsync(object payload, string passphrase)
-    {
-        // serialize the payload to a JSON string
-        var serialized = JsonSerializer.Serialize(payload, payload.GetType());
-
-        // encrypt the byte array
-        var encrypted = await EncryptAsync(serialized, passphrase);
-
-        // convert to base64 for use in cookie
-        return System.Convert.ToBase64String(encrypted);
-    }
-
-    public static async Task<T> DecryptToStringAsync<T>(string payload, string passphrase)
-    {
-        // decode the raw base64 encoded string
-        var decoded = System.Convert.FromBase64String(payload);
-
-        // decrypt to a string
-        var decrypted = await DecryptAsync(decoded, passphrase);
-
-        // deserialize the string
-        return JsonSerializer.Deserialize<T>(decrypted);
-    }
 
     public static async Task<byte[]> EncryptAsync(string plaintext, string passphrase)
     {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Text.Json;
+
+internal class Program
 {
     private static async Task Main(string[] args)
     {
@@ -11,14 +13,38 @@
 
         const string phrase = "my-super-cryptic-passphrase";
 
-        var objectEncoded = await CryptographyHelper.EncryptToStringAsync(dog, phrase);
+        var objectEncoded = await EncryptToBase64StringAsync(dog, phrase);
 
         Console.WriteLine($"Base-64 encoded/encrypted dog: {objectEncoded}");
 
-        var decryptedDog = await CryptographyHelper.DecryptToStringAsync<Dog>(objectEncoded, phrase);
+        var decryptedDog = await DecryptFromBase64StringAsync<Dog>(objectEncoded, phrase);
 
         Console.WriteLine($"Decrypted dog name: {decryptedDog.Name}");
         Console.WriteLine($"Decrypted dog breed: {decryptedDog.Breed}");
         Console.WriteLine($"Decrypted dog age: {decryptedDog.Age}");
+    }
+
+    public static async Task<string> EncryptToBase64StringAsync(object payload, string passphrase)
+    {
+        // serialize the payload to a JSON string
+        var serialized = JsonSerializer.Serialize(payload, payload.GetType());
+
+        // encrypt the byte array
+        var encrypted = await CryptographyHelper.EncryptAsync(serialized, passphrase);
+
+        // convert to base64 for use in cookie
+        return System.Convert.ToBase64String(encrypted);
+    }
+
+    public static async Task<T> DecryptFromBase64StringAsync<T>(string payload, string passphrase)
+    {
+        // decode the raw base64 encoded string
+        var decoded = System.Convert.FromBase64String(payload);
+
+        // decrypt to a string
+        var decrypted = await CryptographyHelper.DecryptAsync(decoded, passphrase);
+
+        // deserialize the string
+        return JsonSerializer.Deserialize<T>(decrypted);
     }
 }
